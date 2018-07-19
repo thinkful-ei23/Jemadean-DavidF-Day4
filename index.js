3eedee9dd848e0d4fca5326fb20211930ca175dc
David Folks <aldwyn.wot@gmail.com>
2018-07-19 12:35:12 -0600

    completed cat carousel

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
+'use strict';
 
+function handleThumbnailClicks() {
+  $('.thumbnail img').on('click', function(event) {
+    const imgSrc = $(this).attr('src');  
+    const imgAlt = $(this).attr('alt');
+    
+    $('.hero img').attr('src', imgSrc).attr('alt', imgAlt);
+    
+  });
+}
 
-$('.thumbnail').click(function (
-  $('.thumbnail').replaceAll('.hero'))
+$(handleThumbnailClicks());
