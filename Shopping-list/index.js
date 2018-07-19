8de49af6c08e221b891495afaff6a6e6c20bd7e0
Jemadean Dobrosielski <jemadean@gmail.com>
2018-07-19 14:59:02 -0700

    only delegation left to go

--- Shopping-list/index.js
+++ Shopping-list/index.js
@@ -18,3 +18,18 @@ function handleDelete() {
 }
 
 $(handleDelete);
+
+function handleSubmit() {
+  $('#js-shopping-list-form').submit(function(event) {
+    event.preventDefault();
+    createListItem ($('.js-shopping-list-entry').val()); 
+  });
+}
+
+function createListItem (submittedItem) {
+  console.log(submittedItem);
+  $('.shopping-list').append(`<li> <span class="shopping-item">${submittedItem}</span> <div class="shopping-item-controls"> <button class="shopping-item-toggle"> <span class="button-label">check</span> </button> <button class="shopping-item-delete"> <span class="button-label">delete</span> </button> </div> </li>`);
+}
+
+$(handleSubmit);
+
